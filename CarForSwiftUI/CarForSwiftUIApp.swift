6968272dac969b0e4c298029cd7be26886bdaa64
Tc_fenghx <Tc_fenghx@yiche.com>
2020-12-21 11:21:56 +0800

    Initial Commit

---
+++ CarForSwiftUI/CarForSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarForSwiftUIApp.swift
+//  CarForSwiftUI
+//
+//  Created by cynfenghanxing on 2020/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CarForSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
